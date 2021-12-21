29d206a43fb45b41ce92d32192bab4502db2f63c
t-lam <lamtranduc2704@gmail.com>
2021-12-21 17:00:19 +0700

    021/12/21

---
+++ jquery-tabledit-add-editable-column-dropdown-option-from-database/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+
+<table id="example2" class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+    </tr>
+    </tbody>
+</table>
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<script src="jquery.tabledit.js"></script>
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+
+
+<script>
+    $(document).ready(function () {
+        $('#example2').Tabledit({
+            editButton: true,
+            removeButton: false,
+            columns: {
+                identifier: [0, 'id'],
+                editable: getData()
+            }
+        });
+
+        async function getData() {
+            const selectData = getSelectData()
+            selectData.done(function(res) {
+                return [[1, 'First Name'],[2, 'Last Name'],[3, 'Username', res]]
+            })
+        }
+
+        function getSelectData() {
+            return $.post('select-data.php')
+        }
+
+        getSelectData()
+    })
+</script>
+
+</body>
+</html>
---
+++ jquery-tabledit-add-editable-column-dropdown-option-from-database/select-data.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: application/json');
+echo json_encode([
+    1 => '@mdo', 2 => '@fat', 3 => '@twitter',
+]);
+
+die();
